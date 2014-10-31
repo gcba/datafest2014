19105e97d0c31c88a6f3d0a2697919e5974add0a
Davo <davidgalavotti@gmail.com>
2014-10-31 19:44:38 -0300

    Ignoro archivos mergeados por exceso de peso
    Former-commit-id: 27b00016dc399b1d038624cc481e5f32ffd3f10a

--- assets/datasets/establecimientos-educativos-brasil/remove-cols.py
+++ assets/datasets/establecimientos-educativos-brasil/remove-cols.py
@@ -1,7 +1,7 @@
 import csv
-creader = csv.reader(open('institucoes-brasileiras-regioes.csv'))
-cwriter = csv.writer(open('institucoes-brasileiras-regioes-edit.csv', 'w'))
-
-for cline in creader:
-   new_line = [val for col, val in enumerate(cline) if col not in (0,2,3,4)]
-   cwriter.writerow(new_line)
+with open("institucoes-brasileiras-regioes.csv","rb") as source:
+   rdr= csv.reader( source, delimiter=';' )
+   with open("institucoes-brasileiras-regioes-edit.csv","wb") as result:
+       wtr= csv.writer( result )
+       for r in rdr:
+           wtr.writerow((r[2], r[5], r[11], r[15], r[16], r[26], r[27]) )
