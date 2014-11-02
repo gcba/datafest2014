c7c77c5f3455aa5fe5171b1d073801c71f9e80a8
Davo <davidgalavotti@gmail.com>
2014-11-02 00:37:05 -0300

    Muevo datos para tratar de pushear
    Former-commit-id: d86514ff7e0b02d1a16006f538f2ba65a42c01a4

--- assets/datasets/establecimientos-educativos-brasil/merge.py
+++
@@ -1,5 +0,0 @@
-fout=open("institucoes-brasileiras-regioes.csv","a")
-for num in range(1,8):
-    for line in open(str(num)+".csv"):
-         fout.write(line)    
-fout.close()
--- assets/datasets/establecimientos-educativos-brasil/remove-cols.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open("institucoes-brasileiras-regioes.csv","rb") as source:
-   rdr= csv.reader( source, delimiter=';' )
-   with open("institucoes-brasileiras-regioes-edit.csv","wb") as result:
-       wtr= csv.writer( result )
-       for r in rdr:
-           wtr.writerow((r[2], r[5], r[11], r[15], r[16], r[26], r[27]) )
